05e7036ccd9d83c039fe5e9afa3d77ec58b1d226
naujkim <juno0218@naver.com>
2021-07-21 08:22:13 -0400

    react-bootstrap use

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { Navbar,Nav,NavDropdown,Container, Card, Button } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+<Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+
+<Card className="background">
+  <Card.Header>20% Season Off</Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+
+
+<div className="container">
+  <div className="row">
+  <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" alt="" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" alt="" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+
+  </div>
+</div>
+
     </div>
   );
 }
