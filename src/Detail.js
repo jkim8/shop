c7811ecb7a6f3edbda2c5b60358ae066be2312df
naujkim <juno0218@naver.com>
2021-07-22 08:10:41 -0400

    useHistory

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 
 function Detail() {
+
+    let history = useHistory()
+
     return (
         <div className="container">
         <div className="row">
@@ -13,6 +17,9 @@ function Detail() {
             <p>상품설명</p>
             <p>120000원</p>
             <button className="btn btn-danger">주문하기</button> 
+            <button className="btn btn-danger" onClick={()=> {
+                history.goBack()
+            }}>뒤로가기</button> 
           </div>
         </div>
   </div> 
