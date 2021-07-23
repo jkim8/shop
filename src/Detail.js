40dc312376a8fd0168d7361eb02ce2077fc01817
naujkim <juno0218@naver.com>
2021-07-23 15:15:45 -0400

    useEffect

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import './Detail.scss'
@@ -12,8 +12,31 @@ let 제목 = styled.h4`
     color : ${ props => props.색상 }
 `
 
+// class Detail2 extends React.Component {
+//   componentDidMount(){
+//     //Detail2 컴포넌트가 Mount 되고나서 실행할 코드
+//   }
+//   componentWillUnmount(){
+//     //Detail2 컴포넌트가 Unmount 되기전에 실행할 코드
+//   }
+// }
 
 function Detail(props) {
+
+  const [Alert, setAlert] = useState(true)
+  const [InputData, setInputData] = useState('')
+
+  useEffect(() => {
+    let 타이머 = setTimeout(() => {
+      setAlert(false)
+    }, 2000);
+    console.log('안녕');
+    return () => {
+      clearTimeout(타이머)
+      // unMound 될때 실행됨  // 사라질때
+    } 
+    
+  }, [])
 
     let { id } = useParams()
     let history = useHistory()
@@ -26,9 +49,23 @@ function Detail(props) {
             <박스>
                 <제목 className='red' 색상="blue">Detail</제목>
             </박스>
+
+        <input onChange={(e)=> {setInputData(e.target.value)}} />
+        { InputData }
+
+        {
+          Alert === true
+          ?
           <div className="my-alert">
             <p>재고가 얼마 남지 않았습니다.</p>
           </div>
+          : null
+        }
+
+
+
+
+
         <div className="row">
           <div className="col-md-6">
             <img src={'https://codingapple1.github.io/shop/shoes'+ (찾은상품.id +1) +'.jpg'} width="100%" alt=""/>
